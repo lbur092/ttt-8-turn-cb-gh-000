aa9d7b252c563b3e3019a010fcf30df72bec0ba0
Laurence Burrows <lbur092@aucklanduni.ac.nz>
2018-02-24 04:07:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,12 @@ def DISPLAY_BOARD(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def VALID_MOVE?(board, index)
+  if index > -1 && index < 9 
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      true
+    else
+      false
+  end
+end
